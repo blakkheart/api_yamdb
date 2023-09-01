222cc30a60337fa7d73d920ff0bc4aa2eaf7b28c
Vlad Y <blakkheart66@gmail.com>
2023-09-01 14:28:34 +0300

    added permission class

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,12 +1,23 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsAdminOrSuperUser(BasePermission):
+class IsAdminOrSuperUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
             request.user.is_superuser or request.user.is_admin
         )
 
 
-class IsAdminOrSuperUserOrAuthor(BasePermission):
-    pass
+class IsAdminModeratorSuperUserAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (request.user.is_superuser
+                or request.user.is_admin
+                or request.user.is_moderator
+                or request.user == obj.author)
