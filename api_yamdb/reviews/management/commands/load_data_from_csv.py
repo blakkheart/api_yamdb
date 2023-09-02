7284b490fa6058fd9d75fc5db1f4890869723a2b
Vlad Y <blakkheart66@gmail.com>
2023-09-02 13:58:07 +0300

    added some beauty

--- api_yamdb/reviews/management/commands/load_data_from_csv.py
+++ api_yamdb/reviews/management/commands/load_data_from_csv.py
@@ -34,14 +34,12 @@ csv_models_dict = {
 class Command(BaseCommand):
     def handle(self, *args, **options) -> str | None:
         for csv_file in list_of_csv:
-            # print(csv_file)
+            print(f'loading {csv_file} ', end='')
             with open(
                 str(settings.CSV_DATA_DIR) + '/' + csv_file + '.csv',
                 encoding='utf-8',
             ) as file:
                 for model_data in csv.DictReader(file):
-                    # print(model_data)
-                    # print(csv_models_dict.get(csv_file))
                     for key, value in model_data.items():
                         if key in csv_models_dict:
                             model_data[key] = csv_models_dict[key].objects.get(
@@ -49,4 +47,6 @@ class Command(BaseCommand):
                             )
                     model = csv_models_dict.get(csv_file)(**model_data)
                     model.save()
+                    print('.', end='')
+            print('.. DONE')
         return
